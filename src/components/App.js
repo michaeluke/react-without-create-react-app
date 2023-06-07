1708c6f3817c8bf19704fcabdec962aa81f0a045
michaeluke <65449279+michaeluke@users.noreply.github.com>
2023-06-07 11:07:08 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React,{Component} from "react";
+// @ts-ignore
+//import {ReactLogo } from 'react-logo';
+
+class App extends Component {
+
+render(){
+
+    return(
+        <div>
+            <h1>Hello! This is react without create react app</h1>
+            
+        </div>
+    )
+}
+
+
+
+}
+
+export default App;
